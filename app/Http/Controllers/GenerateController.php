53f66f03c66cc2c26374ce1e0823c5c81f43cb2b
arafi118 <arp46301@gmail.com>
2026-06-02 09:24:41 +0700

    fix: perbaiki hardcode nama database

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -30,19 +30,9 @@ class GenerateController extends Controller
             $logo = '/storage/logo/' . $kec->logo;
         }
 
-        $database = env('DB_DATABASE', 'siupk_dbm');
         $table = 'pinjaman_kelompok_' . Session::get('lokasi');
 
-        $strukturTabel = \DB::select("
-            SELECT COLUMN_NAME
-            FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE TABLE_NAME = '$table' AND TABLE_SCHEMA='$database'
-            ORDER BY ORDINAL_POSITION;
-        ");
-
-        $struktur = array_map(function ($kolom) {
-            return $kolom->COLUMN_NAME;
-        }, $strukturTabel);
+        $struktur = \Illuminate\Support\Facades\Schema::getColumnListing($table);
 
         return view('generate.index')->with(compact('logo', 'struktur'));
     }
