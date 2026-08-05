a6a28ebaa61abd26dca677e5574c9f4b490dc0d2
arafi118 <arp46301@gmail.com>
2026-08-05 14:02:08 +0700

    fix(tagihan): perkecil image tanda tangan & container agar layout ringkas
    Image dari height=120 menjadi height=80 (lebih proporsional),
    container height=60 menjadi 30. Image 80px di dalam container
    30px dengan valign=bottom overflow 50px ke bawah mengenai nama.
    
    Layout total jauh lebih ringkas:
      sebelumnya ~180px area image
      sekarang       ~80px area image

--- resources/views/perguliran/dokumen/tagihan.blade.php
+++ resources/views/perguliran/dokumen/tagihan.blade.php
@@ -220,9 +220,9 @@
         </tr>
         <tr>
             <td>&nbsp;</td>
-            <td align="center" valign="bottom" height="60">
+            <td align="center" valign="bottom" height="30">
                 @if (file_exists(public_path('assets/img/tagihan/' . $kec->id . '.png')))
-                    <img src="{{ public_path('assets/img/tagihan/' . $kec->id . '.png') }}" height="120" alt="Tanda tangan {{ $dir->namadepan }} {{ $dir->namabelakang }}">
+                    <img src="{{ public_path('assets/img/tagihan/' . $kec->id . '.png') }}" height="80" alt="Tanda tangan {{ $dir->namadepan }} {{ $dir->namabelakang }}">
                 @endif
             </td>
         </tr>
